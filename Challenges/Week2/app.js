5dd15bbcb9342948df8c7b057794b02c03809d92
keithjhinesjr <KeithHinesJr@dhcp013164.code-louisville-wifi.lmcdc.local>
2016-09-08 19:46:50 -0400

    Completing Week 2 JS Challenge

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -3,6 +3,32 @@
 
 //Prompt user for input and store variable
 
+function processInput(message)
+
+{
+  
+    if(message == "hello")
+    {
+        return("What's up?");
+    
+    }
+    
+    
+    else{
+        return("You suck!");
+    }
+    
+    
+}
+
+var userInput = prompt("Hello");
+
+var response = processInput(userInput);
+
+console.log(response);
+
+processInput(userInput);
+
 //Call function
 
 //Alert user results
